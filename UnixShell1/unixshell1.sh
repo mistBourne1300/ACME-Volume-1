902bc44ce152f780aa72761c034248449bbe3325
AlwaysAnotherSecrt <gcford99@gmail.com>
2021-08-31 11:05:09 -0600

    Unix Shell lab completion

--- UnixShell1/unixshell1.sh
+++ UnixShell1/unixshell1.sh
@@ -16,34 +16,45 @@ echo "Done"
 : ' Problem 1: In the space below, write commands to change into the
 Shell1/ directory and print a string telling you the current working
 directory. '
-
+cd Shell1/
+pwd
 
 
 : ' Problem 2: Use ls with flags to print one list of the contents of
 Shell1/, including hidden files and folders, listing contents in long
 format, and sorting output by file size. '
-
+ls -a -l -S
 
 : ' Problem 3: Inside the Shell1/ directory, delete the Audio/ folder
 along with all its contents. Create Documents/, Photos/, and
 Python/ directories. Rename the Random/ folder as Files/. '
-
+rm -rv Audio/ 
+mkdir Documents/
+mkdir Photos/
+mkdir Python/
+mv -v Random/ Files/
 
 
 : ' Problem 4: Using wildcards, move all the .jpg files to the Photos/
 directory, all the .txt files to the Documents/ directory, and all the
 .py files to the Python/ directory. '
-
+mv -v *.jpg Photos/
+mv -v *.txt Documents/
+mv -v *.py Python/
 
 
 : ' Problem 5: Move organize_photos.sh to Scripts/, add executable
 permissions to the script, and run the script. '
+pwd
+mv -v Files/Feb/organize_photos.sh Scripts/
+chmod u+x Scripts/organize_photos.sh
+./Scripts/organize_photos.sh
 
 
 
 : ' Problem 6: Copy img_649.jpg from UnixShell1/ to Shell1/Photos, making
 sure to leave a copy of the file in UnixShell1/.'
-
+cp -v img_649.jpg Shell1/Photos
 
 
 # remove any old copies of UnixShell1.tar.gz
