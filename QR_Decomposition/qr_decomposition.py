947f5012d09fa669b871f211de98d1a51ad328f4
AlwaysAnotherSecret <gcford99@gmail.com>
2021-10-19 10:14:03 -0600

    finished qr_decomp and k means

--- QR_Decomposition/qr_decomposition.py
+++ QR_Decomposition/qr_decomposition.py
@@ -5,6 +5,11 @@
 <Date>
 """
 
+import numpy as np
+import os
+from scipy import linalg as la
+
+sign = lambda x: 1 if x>=0 else -1
 
 # Problem 1
 def qr_gram_schmidt(A):
@@ -17,6 +22,16 @@ def qr_gram_schmidt(A):
         Q ((m,n) ndarray): An orthonormal matrix.
         R ((n,n) ndarray): An upper triangular matrix.
     """
+    m, n = A.shape
+    Q = A.copy()
+    R = np.zeros((n,n))
+    for i in range(n):
+        R[i,i] = np.linalg.norm(Q[:,i])
+        Q[:,i] = Q[:,i]/R[i,i]
+        for j in range(i+1, n):
+            R[i,j] = np.dot(Q[:,j], Q[:,i])
+            Q[:,j] = Q[:,j] - R[i,j]*Q[:,i]
+    return Q,R
     raise NotImplementedError("Problem 1 Incomplete")
 
 
@@ -31,6 +46,11 @@ def abs_det(A):
     Returns:
         (float) the absolute value of the determinant of A.
     """
+    Q,R = qr_gram_schmidt(A)
+    product = 1
+    for i in range(len(R)):
+        product *= R[i,i]
+    return product
     raise NotImplementedError("Problem 2 Incomplete")
 
 
@@ -45,6 +65,13 @@ def solve(A, b):
     Returns:
         x ((n, ) ndarray): The solution to the system Ax = b.
     """
+    Q, R = qr_gram_schmidt(A)
+    y = Q.T @ b
+    n = len(y)
+    x = np.zeros(n)
+    for i in range(1, n+1):
+        x[n-i] = (y[n-i] - np.dot(x, R[n-i,:]))/R[n-i, n-i]
+    return x
     raise NotImplementedError("Problem 3 Incomplete")
 
 
@@ -59,6 +86,17 @@ def qr_householder(A):
         Q ((m,m) ndarray): An orthonormal matrix.
         R ((m,n) ndarray): An upper triangular matrix.
     """
+    m, n = A.shape
+    R = A.copy()
+    Q = np.identity(m)
+    for k in range(n):
+        u = R[k:,k].copy()
+        u[0] = u[0] + sign(u[0]) * np.linalg.norm(u)
+        u = u / np.linalg.norm(u)
+        R[k:, k:] = R[k:, k:] - 2 * np.outer(u, u.T @ R[k:,k:])
+        Q[k:,:] = Q[k:,:] - 2 * np.outer(u, u.T @ Q[k:,:])
+        print("R:\n", R)
+    return Q.T, R
     raise NotImplementedError("Problem 4 Incomplete")
 
 
@@ -74,4 +112,66 @@ def hessenberg(A):
         H ((n,n) ndarray): The upper Hessenberg form of A.
         Q ((n,n) ndarray): An orthonormal matrix.
     """
+    m, n = A.shape
+    H = A.copy()
+    Q = np.identity(m)
+    for k in range(n-2):
+        u = H[k+1:, k].copy()
+        u[0] = u[0] + sign(u[0]) * np.linalg.norm(u)
+        u = u / np.linalg.norm(u)
+        # u_t = u.T@H[k+1:, k:]
+        # np.outer(u,u_t)
+        # H[k+1:,k:] - 2*np.outer(u, u.T@H[k+1:, k:])
+        H[k+1:,k:] = H[k+1:,k:] - 2*np.outer(u, u.T@H[k+1:, k:])
+        H[:, k+1:] = H[:,k+1:] - 2 * np.outer(H[:, k+1:] @ u, u.T)
+        Q[k+1:,:] = Q[k+1:,:] - 2 * np.outer(u, u.T @ Q[k+1:,:])
+        print("H:\n", H)
+    
+    return H, Q.T
     raise NotImplementedError("Problem 5 Incomplete")
+
+if __name__ == "__main__":
+    A = np.random.random((4,4))
+    print("A:\n", A)
+    Q,R = qr_gram_schmidt(A)
+    print("\n\n")
+    print("Q:\n", Q)
+    print("\n\n")
+    print("R:\n", R)
+
+    print("R upper tri: ", np.allclose(np.triu(R), R))
+    print("Q orthonormal: ", np.allclose(Q.T @ Q, np.identity(len(Q))))
+    print("Q @ R = A: ", np.allclose(Q@R, A))
+
+    print("Determinant works: ", np.allclose(abs(np.linalg.det(A)), abs_det(A)))
+
+    print("testing Ax=b solve:")
+    b = np.random.random(4)
+    x = solve(A,b)
+    print("correctly solved: ", np.allclose(A@x, b))
+
+    print("testing householder: ")
+
+    print("A:\n", A)
+    Q,R = qr_householder(A)
+    print("\n\n")
+    print("Q:\n", Q)
+    print("\n\n")
+    print("R:\n", R)
+
+    print("R upper tri: ", np.allclose(np.triu(R), R))
+    print("Q orthonormal: ", np.allclose(Q.T @ Q, np.identity(len(Q))))
+    print("Q @ R = A: ", np.allclose(Q@R, A))
+
+    print("testing hessenberg:")
+
+    print("A:\n", A)
+    H, Q = hessenberg(A)
+    print("\n\n")
+    print("H:\n", H)
+    print("\n\n")
+    print("Q:\n", Q)
+
+    print("Q orthonormal: ", np.allclose(Q.T @ Q, np.identity(len(Q))))
+    print("A = Q@H@Q.T: ", np.allclose(A, Q@H@Q.T))
+
