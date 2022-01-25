d3733fa9b7f2a8044f052eb70a4d553e3e7785a2
AlwaysAnotherSecret <gcford99@gmail.com>
2022-01-25 11:01:43 -0700

    finished newtons method lab

--- NewtonsMethod/newtons_method.py
+++ NewtonsMethod/newtons_method.py
@@ -8,8 +8,10 @@
 from hashlib import new
 import matplotlib.pyplot as plt
 import numpy as np
-import scipy
+from scipy import linalg as la
 from scipy.optimize import newton
+from autograd import grad, jacobian
+from autograd import numpy as anp
 
 
 # Problems 1, 3, and 5
@@ -30,15 +32,27 @@ def newton(f, x0, Df, tol=1e-5, maxiter=15, alpha=1.):
         (bool): Whether or not Newton's method converged.
         (int): The number of iterations computed.
     """
-    x1 = x0 - f(x0)/Df(x0)
-    counter = 1
-    while np.abs(x1-x0) > tol:
-        x0 = x1
-        x1 = x0 - f(x0)/Df(x0)
-        counter += 1
-        if counter > maxiter:
-            return x1, False, counter
-    return x1, True, counter
+    if np.isscalar(x0):
+        x1 = x0 - alpha*f(x0)/Df(x0)
+        counter = 1
+        while np.abs(x1-x0) > tol:
+            x0 = x1
+            x1 = x0 - alpha*f(x0)/Df(x0)
+            counter += 1
+            if counter > maxiter:
+                return x1, False, counter
+        return x1, True, counter
+    else:
+        x1 = x0 - alpha*la.solve(Df(x0), f(x0))
+        counter = 1
+        while la.norm(x1-x0) > tol:
+            x0 = x1
+            x1 = x0 - alpha*la.solve(Df(x0), f(x0))
+            counter += 1
+            if counter > maxiter:
+                return x1, False, counter
+        
+        return x1, True, counter
 
 
 
@@ -61,7 +75,10 @@ def prob2(N1, N2, P1, P2):
     Returns:
         (float): the value of r that satisfies the equation.
     """
-    raise NotImplementedError("Problem 2 Incomplete")
+    f = lambda r: P1*((1 + r)**N1 - 1) - P2*(1-(1+r)**(-N2))
+    df = lambda r: P1*(N1*(1+r)**(N1-1)) - P2*(N2*(1+r)**(-N2-1))
+    return newton(f, 0.1, df)[0]
+
 
 
 # Problem 4
@@ -81,7 +98,12 @@ def optimal_alpha(f, x0, Df, tol=1e-5, maxiter=15):
         (float): a value for alpha that results in the lowest number of
             iterations.
     """
-    raise NotImplementedError("Problem 4 Incomplete")
+    alphas = np.linspace(0.001,1, num = 100, endpoint=False)
+    newtons_cradle = np.vectorize(newton, excluded = ['f', 'x0', 'Df', 'tol', 'maxiter'])
+    plt.plot(alphas, newtons_cradle(f, x0, Df, tol, maxiter, alphas)[2])
+    plt.show()
+    return alphas[np.argmin(newtons_cradle(f,x0,df, tol, maxiter, alphas)[2])]
+
 
 
 # Problem 6
@@ -95,7 +117,25 @@ def prob6():
     (0,1) or (0,−1) with alpha = 1, and to (3.75, .25) with alpha = 0.55.
     Return the intial point as a 1-D NumPy array with 2 entries.
     """
-    raise NotImplementedError("Problem 6 Incomplete")
+    f = lambda x: anp.array([5*x[1]*x[0] - x[0]*(1+x[1]), -x[0]*x[1] + (1-x[1])*(1 + x[1])])
+    df = jacobian(f)
+    exxes = np.linspace(-1/4., -.01)
+    whys = np.linspace(0.01,1/4.)
+    zero55 = np.array([3.75, .25])
+    zero11 = np.array([0,1])
+    zero12 = np.array([0,-1])
+    x0 = None
+    for x in exxes:
+        for y in whys:
+            x0 = np.array([x,y])
+            print(f'testing {x0}')
+            try:
+                if (np.allclose(newton(f, x0, df, alpha = 1)[0], zero11) or np.allclose(newton(f, x0, df, alpha = 1)[0], zero12)) and np.allclose(newton(f, x0, df, alpha = .55)[0], zero55):
+                    return x0
+            except:
+                print("\tmatrix was singular")
+                continue
+
 
 
 # Problem 7
@@ -112,16 +152,72 @@ def plot_basins(f, Df, zeros, domain, res=1000, iters=15):
             The visualized grid has shape (res, res).
         iters (int): The exact number of times to iterate Newton's method.
     """
-    raise NotImplementedError("Problem 7 Incomplete")
+    x_real = np.linspace(domain[0], domain[1], res)
+    x_imag = np.linspace(domain[2], domain[3], res)
+    XREAL, XIMAG = np.meshgrid(x_real, x_imag)
+    X0 = XREAL + 1j*XIMAG
+    for i in range(iters):
+        X0 = X0 - f(X0)/Df(X0)
+    
+    y = np.zeros((res,res))
+
+    for i in range(res):
+        for j in range(res):
+            vals = np.abs(zeros - X0[i,j])
+            y[i,j] = np.argmin(vals)
+    
+    plt.pcolormesh(y, cmap = 'Dark2')
+    plt.show()
+            
+
+
+
+def test5():
+    f = lambda x: anp.array((x[0]*x[1],x[0]-x[1]))
+    Df = jacobian(f)
+    x0 = np.array((0.1,0.1))
+    print(newton(f,x0,Df,maxiter=30))
 
 
 if __name__ == "__main__":
-    f = lambda x: x**5 - 3
-    df = lambda x: 5*x**4
+    # f = lambda x: x**5 - 3
+    # df = lambda x: 5*x**4
 
-    soln = newton(f, 1, df, maxiter=100, tol = 1e-10)
-    print(soln)
-    print(f'f(soln): {f(soln[0])}')
-    # domain = np.linspace(-1,2)
-    # plt.plot(domain, f(domain), 'k')
-    # plt.show()
+    # soln = newton(f, 1, df, maxiter=100, tol = 1e-10)
+    # print(soln)
+    # print(f'f(soln): {f(soln[0])}')
+    # # domain = np.linspace(-1,2)
+    # # plt.plot(domain, f(domain), 'k')
+    # # plt.show()
+
+
+    # print(prob2(30, 20, 2000, 8000))
+
+
+
+    f = lambda x: np.sign(x) * np.power(np.abs(x), 1./3)
+    df = grad(f)
+    # print(f'a = 1: {newton(f, 0.1, df)}')
+    # print(f'a = .4: {newton(f, 0.1, df, alpha = .4)}')
+
+    # print(optimal_alpha(f, .1, df))
+
+    f = lambda x: np.array([x[0]**2 + x[1]**2, x[0]+x[1]])
+
+    # test5()
+
+    # x0 = prob6()
+    # f = lambda x: anp.array([5*x[1]*x[0] - x[0]*(1+x[1]), -x[0]*x[1] + (1-x[1])*(1 + x[1])])
+    # df = jacobian(f)
+    # print(f'.55: {newton(f,x0,df, alpha = .55)}\n1:{newton(f,x0,df)}')
+
+    # This is how I called problem 7 to test.
+    f = lambda x: x**3 -1
+    Df = lambda x: 3*x**2
+
+    plot_basins(f,Df,np.array((1,
+            -.5+.866025j,-.5-.866025j
+    )),[-1,1,-1,1])
+    # This will give you the zeros and the derivative without Autograd
+
+    pass
