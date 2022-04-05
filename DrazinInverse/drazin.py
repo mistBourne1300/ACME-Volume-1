d21110454e74531031043e0b2392ba184e34d40a
AlwaysAnotherSecret <gcford99@gmail.com>
2022-04-05 17:40:42 -0600

    update as of 2022-04-05

--- DrazinInverse/drazin.py
+++ DrazinInverse/drazin.py
@@ -49,11 +49,15 @@ def is_drazin(A, Ad, k):
     Returns:
         (bool) True of Ad is the Drazin inverse of A, False otherwise.
     """
+    # check A@Ad == Ad@A
     if not np.allclose(A@Ad, Ad@A):
         return False
+    
+    # check Ad@A@Ad == Ad
     if not np.allclose(Ad@A@Ad,Ad):
         return False
     
+    # check (A**k+1)@Ad == A**k
     Ak = np.linalg.matrix_power(A,k)
     if not np.allclose(A@Ak@Ad,Ak):
         return False
@@ -70,15 +74,25 @@ def drazin_inverse(A, tol=1e-4):
     Returns:
        ((n,n) ndarray) The Drazin inverse of A.
     """
+    # get shape
     n = A.shape[0]
+    # define "zero" functions
     f = lambda x: abs(x) > tol
     g = lambda x: abs(x) <= tol
+
+    # get T1,Q1,k1 and T2,Q2,k2
     T1,Q1,k1 = la.schur(A,sort = f)
     T2,Q2,k2 = la.schur(A,sort = g)
+
+    # stack Q1 and Q2 (up to the relevant index) to create U
     U = np.hstack((Q1[:,:k1],Q2[:,:n-k1]))
+    # get U inverse
     U_1 = la.inv(U)
+    # create V and initialize Z
     V = U_1@A@U
     Z = np.zeros((n,n))
+
+    # if necessary, remake Z to be int inverse of V
     if k1 != 0:
         Z[:k1,:k1] = la.inv(V[:k1,:k1])
     return U@Z@U_1
@@ -94,10 +108,14 @@ def effective_resistance(A):
         ((n,n) ndarray) The matrix where the ijth entry is the effective
         resistance from node i to node j.
     """
-    n,m = A.shape
+    # create lapplacian
     laplacian = np.diag(A.sum(axis = 1)) - A
+    # initialize R and create I
     R = np.zeros(A.shape)
     I = np.eye(A.shape[0])
+
+    # for each column, copy the laplacian and replace the column with the identity
+    # Get the Drazin inverse, and push the diagonals to the column of R
     for j in range(A.shape[0]):
         Lj = laplacian.copy()
         Lj[j,:] = I[j,:]
@@ -116,14 +134,20 @@ class LinkPredictor:
         Parameters:
             filename (str): The name of a file containing graph data.
         """
+        # go through the file, pushing each name into the set of names 
         names = set()
         with open(filename) as file:
             for line in file.readlines():
                 for name in line.strip().split(','):
                     names.add(name)
+        # make names a list and sort it
         self.names = list(names)
         self.names.sort()
+        
+        # initialize Adjacency as zeros
         self.Adj = np.zeros((len(names),len(names)))
+
+        # go through the file again, adding adjacencies between the names
         with open(filename) as file:
             for line in file.readlines():
                 name1,name2 = line.strip().split(',')
@@ -131,7 +155,8 @@ class LinkPredictor:
                 self.Adj[i,j] += 1
                 self.Adj[j,i] += 1
         
-        self.ER = effective_resistance(A)
+        # create the resistance matrix
+        self.ER = effective_resistance(self.Adj)
 
     def predict_link(self, node=None):
         """Predict the next link, either for the whole graph or for a
@@ -149,17 +174,31 @@ class LinkPredictor:
         Raises:
             ValueError: If node is not in the graph.
         """
-        if node:
-            ER = self.ER.copy()
-            Adj = self.Adj.copy()
+        # copy ER and the Adjacency matrix
+        ER = self.ER.copy()
+        Adj = self.Adj.copy()
+
+        # create a mask where there are connections in the Adjacency matrix
+        # then make those entries zero in ER
+        mask = Adj!=0
+        ER[mask] = 0
+
+        # if node was not passed in, get the overall closest non-linked people
+        if not node:
+            # get the nonzero minimum
+            minimum = np.min(ER[np.nonzero(ER)])
+            # get the index of the nonzero minimum and return
+            row,col = np.where(ER == minimum)
+            return self.names[row[0]],self.names[col[0]]
+        
+        else:
+            # get the appropriate row corresponding to node
             i = self.names.index(node)
-            mask = Adj != 0
-            ER[mask] = 0
-            minimum = np.min(ER[i,:]!=0)
-            argmin = np.where(ER[i,:] == minimum)
-            return self.names[argmin]
-        print(np.argmin(self.ER))
-        raise NotImplementedError("Problem 5 Incomplete")
+            # get the nonzero minimum along the correct row
+            minimum = np.min(ER[i,np.nonzero(ER[i,:])])
+            # get index onf minimum
+            ind = np.where(ER[i,:] == minimum)
+            return self.names[ind[0][0]]
 
     def add_link(self, node1, node2):
         """Add a link to the graph between node 1 and node 2 by updating the
@@ -172,7 +211,14 @@ class LinkPredictor:
         Raises:
             ValueError: If either node1 or node2 is not in the graph.
         """
-        raise NotImplementedError("Problem 5 Incomplete")
+        # get row and column indices
+        row = self.names.index(node1)
+        col = self.names.index(node2)
+        # create adjacencies between node1 and node2
+        self.Adj[row,col] = 1
+        self.Adj[col,row] = 1
+        # recalculate resistance matrix
+        self.ER = effective_resistance(self.Adj)
 
 
 if __name__ == "__main__":
@@ -209,4 +255,13 @@ if __name__ == "__main__":
     print(effective_resistance(graph1))
 
     print("\n\nPROBLEM 4:\n")
-    print(LinkPredictor().predict_link('Melanie'))
+    Zelda = LinkPredictor()
+    print(f'overall: {Zelda.predict_link()}')
+    print(f'Melanie: {Zelda.predict_link("Melanie")}')
+    print(f'Alan: {Zelda.predict_link("Alan")}')
+    Zelda.add_link("Alan", "Sonia")
+    print("added link between Alan and Sonia")
+    print(f'Alan: {Zelda.predict_link("Alan")}')
+    Zelda.add_link("Alan", "Piers")
+    print("added link between Alan and Piers")
+    print(f'Alan: {Zelda.predict_link("Alan")}')
